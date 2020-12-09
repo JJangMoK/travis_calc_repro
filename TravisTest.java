5aaf721590b6dbc272aa48589523efd6491446b3
JJangMoK <70704620+JJangMoK@users.noreply.github.com>
2020-12-09 15:59:43 +0900

    Create TravisTest.java
    d

---
+++ TravisTest.java
@@ -0,0 +1,12 @@
+public class TravisTest {
+  private int num1;
+  private int num2;
+  private int val;
+  
+  public int Multiple(int a, int b) {
+    num1 = a;
+    num2 = b;
+    val = num1 * num2;
+    return val;
+    }
+}
